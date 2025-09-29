6f38391d07c5d9054e17acf280f558f086c44d9f
Sunami97 <sunami7191@daum.net>
2025-09-29 10:58:37 +0900

    feat: test

--- PMS/src/main/java/org/pms/feature/payrollvoucher/PayrollVoucherController.java
+++ PMS/src/main/java/org/pms/feature/payrollvoucher/PayrollVoucherController.java
@@ -1,5 +1,13 @@
 package org.pms.feature.payrollvoucher;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/runpayroll/api/voucher")
+@RequiredArgsConstructor
 public class PayrollVoucherController {
 
 }
