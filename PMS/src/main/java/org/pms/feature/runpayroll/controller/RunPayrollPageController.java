025b82f20e017cbcc085f3c96f198a3166d0f224
Sunami97 <sunami7191@daum.net>
2025-10-12 11:36:51 +0900

    fix: 사용하지 않는 클래스 제거

--- PMS/src/main/java/org/pms/feature/runpayroll/controller/RunPayrollPageController.java
+++ PMS/src/main/java/org/pms/feature/runpayroll/controller/RunPayrollPageController.java
@@ -12,13 +12,6 @@ import java.time.format.DateTimeFormatter;
 @RequiredArgsConstructor
 public class RunPayrollPageController {
 
-    /**
-     * 급상여 처리 메인 페이지
-     * - JSP 경로 규칙: /WEB-INF/view/runpayroll/runpayroll.jsp  (예시)
-     * - ViewResolver 설정 예:
-     *   spring.mvc.view.prefix=/WEB-INF/view/
-     *   spring.mvc.view.suffix=.jsp
-     */
     @GetMapping({"/runpayroll", "/runpayroll/"})
     public String runPayrollPage(Model model) {
         // 기본값: 이번달, SALARY
@@ -29,11 +22,6 @@ public class RunPayrollPageController {
         model.addAttribute("defaultYyyymm", defaultYyyymm);
         model.addAttribute("defaultPayType", defaultPayType);
 
-        // 필요시 화면 좌측 검색용 기본 부서코드/사번 등도 추가 가능
-        // model.addAttribute("defaultDeptCode", "");
-        // model.addAttribute("defaultEmpNo", "");
-
-        // /WEB-INF/view/runpayroll/runpayroll.jsp
         return "runpayroll/list";
     }
 }
